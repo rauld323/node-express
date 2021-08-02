b40bcd5a8fa41b4090fb2fd83b1ff592474f60aa
Raul <rauldavila323@gmail.com>
2021-08-02 12:03:52 +0200

    Exer. 2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello Express');
+});
+
+// Routes start with the structure 'app.METHOD(PATH, HANDLER)'
+
+// Method could be GET, Post, PATCH, Delete in lowercase.
+
+// The PATH is the path where you will sent. This could be / /posts /blogs /products .app
+
+// The handler is called when the route getMatchedCSSRules.
+
+module.exports = app;
